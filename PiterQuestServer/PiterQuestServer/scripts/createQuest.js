24c4626290be7c3ae0fb523e7b8e9588e7f59f20
Sharganov Artem <sharganov.artem@yandex.ru>
2016-04-17 12:03:18 +0300

    pre-Final version

--- PiterQuestServer/PiterQuestServer/scripts/createQuest.js
+++ PiterQuestServer/PiterQuestServer/scripts/createQuest.js
@@ -1,8 +1,8 @@
-
+var map;
  
 function initialize() {
     var mapProp = {
-      center:new google.maps.LatLng(59.9386300,30.3141300),
+        center: new google.maps.LatLng(59.891434, 30.319433),
       zoom:12,
       mapTypeId:google.maps.MapTypeId.ROADMAP
     };
@@ -18,7 +18,7 @@ function addMarker()
 	 var nlng = parseFloat($("#inputCoor2").val());
 	 var myLatLng = {lat: nlat, lng: nlng};
 	 var mapProp = {
-      center:new google.maps.LatLng(59.9386300,30.3141300),
+	     center: new google.maps.LatLng(59.891434, 30.319433),
       zoom:12,
       mapTypeId:google.maps.MapTypeId.ROADMAP
     };
@@ -35,8 +35,8 @@ function addMarker()
   
   function placeMarker(location) {
 	  var mapProp = {
-      center:new google.maps.LatLng(59.9386300,30.3141300),
-      zoom:12,
+	      center: new google.maps.LatLng(59.891434, 30.319433),
+      zoom:13,
       mapTypeId:google.maps.MapTypeId.ROADMAP
     };
     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
@@ -52,18 +52,12 @@ function addMarker()
 	google.maps.event.addListener(map, 'click', function(event) {
     placeMarker(event.latLng);
 	}); 
-}
-
-function roundPlus(x, n) { //x - число, n - количество знаков
-
-	//if(isNaN(x) || isNaN(n)) return false;
-	var m = Math.pow(10,n);
-	return Math.round(x*m)/m;
-
-}
+  }
 
 function removeAllElems()
 {
 	
-	$("#questList").html("");
+    $("#questList").html("");
+    map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
 }
