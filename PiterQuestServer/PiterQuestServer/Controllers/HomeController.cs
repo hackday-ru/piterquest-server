1f7d20a9c60794207e0511667cfe8c515a780b61
SergeevED <sergeev.ev.dm@gmail.com>
2016-04-17 11:07:47 +0300

    added login

--- PiterQuestServer/PiterQuestServer/Controllers/HomeController.cs
+++ PiterQuestServer/PiterQuestServer/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Web.Mvc;
 using Quests.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Quests.Controllers
 {
@@ -18,5 +20,42 @@ namespace Quests.Controllers
       ViewBag.quests = context.Quests;
       return View();
     }
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        public ActionResult Login(string login, string password)
+        {
+            var result = false;
+            using (MD5 md5Hash = MD5.Create())
+            {
+                if (login == "admin" && GetMd5Hash(md5Hash, password) == "912e79cd13c64069d91da65d62fbb78c")
+                {
+                    result = true;
+                }
+            }
+
+                
+            
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
   }
 }
